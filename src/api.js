50c523f1378eb4717fc4509271ed17ce8392a716
mansoor-nouman <54888881+mansoor-nouman@users.noreply.github.com>
2021-07-01 12:04:01 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export function Userdetails(data)
 {
-    return axios.post("http://localhost:5000/Registration",data)
+    return axios.post("http://regformnode.herokuapp.com/Registration",data)
 }
 
 export function GetResult(){
-    return axios.get(`http://localhost:5000/Result`)
+    return axios.get(`http://regformnode.herokuapp.com/Result`)
 }
 
 export function Deleteuser(id){
-    return axios.delete(`http://localhost:5000/Delete/${id}`)
+    return axios.delete(`http://regformnode.herokuapp.com/Delete/${id}`)
 }
